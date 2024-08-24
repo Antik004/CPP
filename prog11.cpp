8255c244ddf41254a90341ab935775388637d4cf
Antik Adhya <adhyaantik03@gmail.com>
2024-08-24 21:29:52 +0530

    commit

--- prog11.cpp
+++ prog11.cpp
@@ -1,3 +1,4 @@
+//DEFAULT FUNCTIONS & CONSTANT ARGUMENTS 
 #include <iostream>
 using namespace std;
 int product(int a,int b)
@@ -12,14 +13,23 @@ float moneyReceived(int currentMoney,float factor=1.04)
 {
     return currentMoney*factor;
 }
+int strlength(const string str)
+{
+    return str.size(); 
+}
 int main()
 {
     int a,b;
+    string s;
     cout<<"Enter the value of a and b"<<endl;
     cin>>a;
     cin>>b;
     cout<<"The product of a and b is "<<product(a,b)<<endl;  
     int money=100000;
     cout<<"If we have "<<money<<" in our bank acc, we will receive "<<moneyReceived(money)<<" Rs after 1 year"<<endl;
+    cout<<"Enter string"<<endl;
+    cin>>s;
+    cout<<strlength(s)<<endl;
+     
     return 0;
 }
