0072f685ac393d636f220460e37119fc6b551c2f
Antik Adhya <adhyaantik03@gmail.com>
2024-08-24 20:58:37 +0530

    commit

---
+++ Name.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"This is just a test program"<<endl;
+    //checking auto-commit is working on not...its just a sample program
+
+}//:)
