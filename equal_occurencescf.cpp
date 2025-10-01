7a5fa0f96866ec5095d983cc2cfd0066a22f2e7a
Antik004 <adhyaantik03@gmail.com>
2025-10-01 07:57:25 +0530

    commit

--- equal_occurencescf.cpp
+++ equal_occurencescf.cpp
@@ -1,17 +1,56 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
 int main(){
-    int n;
-    vector<int> arr(n);
+
+    
     int tc;
     cin>>tc;
     for(int i=0;i<tc;i++)
     {
+        int n;
         cin>>n;
-        
-    }
-    
+        vector<int> arr(n);
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[j];
+        }
 
+        unordered_map<int ,int>freq;
+
+        for(int k=0;k<n;k++)
+        {
+            freq[arr[k]]++;
+        }
+        int maxFreq=0;
+
+        for(auto&p:freq)
+        {
+            if(p.second>maxFreq)
+            {
+                maxFreq=p.second;
+            }
+        }
+        int maxLen=0;
+        for(int l=1;l<=maxFreq;l++)
+        {
+            int count=0;
+            for(auto&p:freq)
+            {
+                if(p.second>=l)
+                {
+                    count++;
+                }
+            }
+            int length=count*l;
+            if(length>maxLen)
+            {
+                maxLen=length;
+            }
+        }
+        cout<<maxLen<<endl;
+    }
+    return 0;
 }
